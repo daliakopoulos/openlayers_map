a945dc1087b594196323d6eac3736bef4ae2b11e
kokadva <konstantine.dvalishvil@gmail.com>
2022-12-23 20:57:13 +0400

    Git commit -m "initial commit"

---
+++ main.js
@@ -0,0 +1,33 @@
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import {DrawPolygon} from './draw'
+import {RasterStatistics} from "./raster";
+import {Vector as VectorSource} from "ol/source";
+import {Vector as VectorLayer} from "ol/layer";
+
+
+
+const map = new Map({
+    target: 'map',
+    view: new View({
+        center: [-11000000, 4600000],
+        zoom: 15,
+    }),
+});
+
+const source = new VectorSource();
+
+const vector = new VectorLayer({
+    source: source,
+    style: {
+        'fill-color': 'rgba(255, 255, 255, 0.2)',
+        'stroke-color': '#ffcc33',
+        'stroke-width': 2,
+        'circle-radius': 7,
+        'circle-fill-color': '#ffcc33',
+    },
+});
+
+const rasterStatistics = new RasterStatistics(map, vector);
+const draw = new DrawPolygon(map, vector);
+draw.onDrawn(rasterStatistics.plot)
---
+++ raster.js
@@ -0,0 +1,231 @@
+import RasterSource from 'ol/source/Raster.js';
+import XYZ from 'ol/source/XYZ.js';
+import {Image as ImageLayer, Tile as TileLayer} from 'ol/layer.js';
+
+
+const key = 'U4TB1Bcgau3wRomRNAcU';
+const attributions =
+    '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+    '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+
+const aerial = new XYZ({
+    attributions: attributions,
+    url: 'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=' + key,
+    maxZoom: 20,
+    crossOrigin: '',
+
+});
+
+const raster = new RasterSource({
+    sources: [aerial],
+    /**
+     * Run calculations on pixel data.
+     * @param {Array} pixels List of pixels (one per source).
+     * @param {Object} data User data object.
+     * @return {Array} The output pixel.
+     */
+    operation: function (pixels, data) {
+        const pixel = pixels[0];
+        const r = pixel[0] / 255;
+        const g = pixel[1] / 255;
+        const b = pixel[2] / 255;
+        const value =  (2 * g - r - b) / (2 * g + r + b);
+        const counts = data.counts;
+        const min = counts.min;
+        const max = counts.max;
+        const num = counts.values.length;
+        if (value < min) {
+            // do nothing
+        } else if (value >= max) {
+            counts.values[num - 1] += 1;
+        } else {
+            const index = Math.floor((value - min) / counts.delta);
+            counts.values[index] += 1;
+        }
+        if (value >= data.threshold) {
+            pixel[0] = 0;
+            pixel[1] = 255;
+            pixel[2] = 0;
+            pixel[3] = 128;
+        } else {
+            pixel[3] = 0;
+        }
+        return pixel;
+    },
+    lib: {
+        vgi: (pixel) => {
+            const r = pixel[0] / 255;
+            const g = pixel[1] / 255;
+            const b = pixel[2] / 255;
+            return (2 * g - r - b) / (2 * g + r + b);
+        },
+        summarize: summarize,
+    },
+});
+
+const minVgi = 0;
+const maxVgi = 0.5;
+const bins = 10;
+
+function mapping(pixel) {
+    const r = pixel[0] / 255;
+    const g = pixel[1] / 255;
+    const b = pixel[2] / 255;
+    return (2 * g - r - b) / (2 * g + r + b);
+}
+
+function summarize(value, counts) {
+    const min = counts.min;
+    const max = counts.max;
+    const num = counts.values.length;
+    if (value < min) {
+        // do nothing
+    } else if (value >= max) {
+        counts.values[num - 1] += 1;
+    } else {
+        const index = Math.floor((value - min) / counts.delta);
+        counts.values[index] += 1;
+    }
+}
+
+function createCounts(min, max, num) {
+    const values = new Array(num);
+    for (let i = 0; i < num; ++i) {
+        values[i] = 0;
+    }
+    return {
+        min: min,
+        max: max,
+        values: values,
+        delta: (max - min) / num,
+    };
+}
+
+let timer = null;
+function schedulePlot(resolution, counts, threshold) {
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+    }
+    timer = setTimeout(plot.bind(null, resolution, counts, threshold), 1000 / 60);
+}
+
+const barWidth = 15;
+const plotHeight = 150;
+const chart = d3
+    .select('#plot')
+    .append('svg')
+    .attr('width', barWidth * bins)
+    .attr('height', plotHeight);
+
+const chartRect = chart.node().getBoundingClientRect();
+const tip = d3.select(document.body).append('div').attr('class', 'tip');
+
+function plot(resolution, counts, threshold) {
+    const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(counts.values)])
+        .range([0, plotHeight]);
+
+    const bar = chart.selectAll('rect').data(counts.values);
+
+    bar.enter().append('rect');
+
+    bar
+        .attr('class', function (count, index) {
+            const value = counts.min + index * counts.delta;
+            return 'bar' + (value >= threshold ? ' selected' : '');
+        })
+        .attr('width', barWidth - 5);
+
+    bar
+        .transition()
+        .attr('transform', function (value, index) {
+            return (
+                'translate(' +
+                index * barWidth +
+                ', ' +
+                (plotHeight - yScale(value)) +
+                ')'
+            );
+        })
+        .attr('height', yScale);
+
+    bar.on('mousemove', function () {
+        const index = bar.nodes().indexOf(this);
+        const threshold = counts.min + index * counts.delta;
+        if (raster.get('threshold') !== threshold) {
+            raster.set('threshold', threshold);
+            raster.changed();
+        }
+    });
+
+    bar.on('mouseover', function (event) {
+        const index = bar.nodes().indexOf(this);
+        let area = 0;
+        for (let i = counts.values.length - 1; i >= index; --i) {
+            area += resolution * resolution * counts.values[i];
+        }
+        tip.html(message(counts.min + index * counts.delta, area));
+        tip.style('display', 'block');
+        tip
+            .transition()
+            .style('left', chartRect.left + index * barWidth + barWidth / 2 + 'px')
+            .style('top', event.y - 60 + 'px')
+            .style('opacity', 1);
+    });
+
+    bar.on('mouseout', function () {
+        tip
+            .transition()
+            .style('opacity', 0)
+            .on('end', function () {
+                tip.style('display', 'none');
+            });
+    });
+}
+
+function message(value, area) {
+    const acres = (area / 4046.86)
+        .toFixed(0)
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return acres + ' acres at<br>' + value.toFixed(2) + ' VGI or above';
+}
+
+
+
+const rasterLayer = new ImageLayer({
+    source: raster,
+});
+
+export class RasterStatistics {
+
+    constructor(map) {
+        this.map = map;
+
+        raster.set('threshold', 0.25);
+
+        raster.on('beforeoperations', function (event) {
+            event.data.counts = createCounts(minVgi, maxVgi, bins);
+            event.data.threshold = raster.get('threshold');
+        });
+
+        raster.on('afteroperations', function (event) {
+            schedulePlot(event.resolution, event.data.counts, event.data.threshold);
+        });
+
+        map.addLayer(new TileLayer({
+            source: aerial,
+        }))
+        rasterLayer.setVisible(false);
+        map.addLayer(rasterLayer)
+    }
+
+    plot(sketch) {
+        // const extent = transform([68.17665, 7.96553, 97.40256, 35.49401])
+        const extent = sketch.getGeometry().getExtent()
+        console.log(extent);
+        rasterLayer.setExtent(extent);
+        rasterLayer.setVisible(true);
+    }
+}
