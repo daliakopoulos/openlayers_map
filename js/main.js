78043d16cf94c1610c7507df3d14bd33255e975a
kokadva <konstantine.dvalishvil@gmail.com>
2022-12-24 17:42:09 +0400

    Small reff

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ import Map from 'ol/Map.js';
 import View from 'ol/View.js';
 import {DrawPolygon} from './draw'
 import {RasterStatistics} from "./raster";
-import {Vector as VectorSource} from "ol/source";
-import {Vector as VectorLayer} from "ol/layer";
 
 
 const map = new Map({
@@ -14,20 +12,9 @@ const map = new Map({
     }),
 });
 
-const source = new VectorSource();
 
-const vector = new VectorLayer({
-    source: source,
-    style: {
-        'fill-color': 'rgba(255, 255, 255, 0.2)',
-        'stroke-color': '#ffcc33',
-        'stroke-width': 2,
-        'circle-radius': 7,
-        'circle-fill-color': '#ffcc33',
-    },
-});
+const rasterStatistics = new RasterStatistics(map);
 
-const rasterStatistics = new RasterStatistics(map, vector);
-const draw = new DrawPolygon(map, vector);
+const draw = new DrawPolygon(map);
 draw.onDrawEnd((x) => rasterStatistics.plot.call(rasterStatistics, x))
 draw.onDrawStart((x) => rasterStatistics.clear.call(rasterStatistics, x))
